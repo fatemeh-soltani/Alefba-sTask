3a1d1397ba4cbdb475a2bdcfa5d40da7bb22695d
fatemeh-soltani <55808259+fatemeh-soltani@users.noreply.github.com>
2021-09-15 20:22:39 +0430

    1
    1

---
+++ src/core/services/story.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StoryService {
+  constructor(private http: HttpClient) {}
+}

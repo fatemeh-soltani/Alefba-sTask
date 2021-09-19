8998401327190e5e6fbae3777f080ee1bf1aa7aa
fatemeh-soltani <55808259+fatemeh-soltani@users.noreply.github.com>
2021-09-19 13:07:03 +0430

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { NewsService } from '../core/services/news.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'alefba';
+  constructor(private api: NewsService) {}
+  ngOnInit() {
+    this.api.apiCall().subscribe((data) => {
+      console.log('get api: ', data);
+    });
+    console.log('hi');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsComponent } from './news/news.component';
 
-
 @NgModule({
   declarations: [AppComponent, NewsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
